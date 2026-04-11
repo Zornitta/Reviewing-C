d6315050f35ebdf462e252dc1c967ee9a4b8864c
Zornitta <lucaszornitta2@gmail.com>
2026-04-11 12:52:51 -0300

    sixth_commit

--- Programação Básica em C/Aula_14/Atividade_6.c
+++ Programação Básica em C/Aula_14/Atividade_6.c
@@ -4,10 +4,30 @@ raiz quadrada é exata e 0 em caso contrário.
 */
 
 #include <stdio.h>
+#include <math.h>
 
-
+int exact_root(int n)
+{
+    if (n < 0) return 0;
+    int root = (int)sqrt(n);
+    return root * root == n;
+}
 
 int main(void)
 {
+    int number;
 
+    printf("\nInsert number:\n... ");
+    scanf("%d", &number);
+
+    if (exact_root(number))
+    {
+        printf("\nYour square root is exact!");
+    }
+    else
+    {
+        printf("\nYour square root is not exact!");
+    }
+
+    return 0;
 }
